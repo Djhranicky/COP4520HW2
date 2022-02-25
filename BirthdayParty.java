cddf90d6ae117d23bf4cab91c0f52b58c9239c0a
David Hranicky <djh30000@gmail.com>
2022-02-25 10:48:39 -0500

    Added java files, updated .gitignore

---
+++ BirthdayParty.java
@@ -0,0 +1,8 @@
+// David Hranicky
+// COP4520
+
+class BirthdayParty{
+    public static void main(String[] args){
+
+    }
+}
---
+++ CrystalVase.java
@@ -0,0 +1,7 @@
+// David Hranicky
+// COP4520
+
+class CrystalVase{
+    public static void main(String[] args)
+
+}

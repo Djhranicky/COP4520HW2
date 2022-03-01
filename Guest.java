0fd965fd089c10b955edb9bbc5bebc4bb41fbdd1
David Hranicky <djh30000@gmail.com>
2022-03-01 18:32:53 -0500

    Added local vars, non-leader's logic implemented

--- Guest.java
+++ Guest.java
@@ -1,11 +1,46 @@
-public class Guest implements Runnable{
-    
-    public Guest(){
+import java.util.concurrent.atomic.AtomicBoolean;
 
+public class Guest implements Runnable{
+
+    enum State{
+        WAITING, INMAZE, WIN
+    };
+    
+    private State state;
+    private Boolean replaced;
+    private int count;
+    int numGuests;
+    Boolean leader;
+    AtomicBoolean cupcake;
+
+    public Guest(Boolean leader, AtomicBoolean cupcake, int numGuests){
+        this.leader = leader;
+        this.cupcake = cupcake;
+        this.numGuests = numGuests;
+        this.state = State.WAITING;
+        this.replaced = false;
+    }
+
+    public void setState(State state){
+        this.state = state;
     }
 
     @Override
     public void run(){
-        
+        while(state != State.WIN){
+            if(leader){
+                if(count)
+            }
+            else{
+                if(state == State.WAITING){
+                    continue;
+                }
+                else if(state == State.INMAZE){
+                    if(replaced == false && cupcake.compareAndSet(false, true)){
+                        replaced = true;
+                    }
+                }
+            }
+        }
     }
 }

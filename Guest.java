15dd2d23370263fd853be99772b46ba03ee9d3b9
David Hranicky <djh30000@gmail.com>
2022-03-01 20:10:50 -0500

    Added getState

--- Guest.java
+++ Guest.java
@@ -22,6 +22,10 @@ public class Guest implements Runnable{
         this.count = 0;
     }
 
+    public State getState(){
+        return this.state;
+    }
+
     public void setState(State state){
         this.state = state;
     }
